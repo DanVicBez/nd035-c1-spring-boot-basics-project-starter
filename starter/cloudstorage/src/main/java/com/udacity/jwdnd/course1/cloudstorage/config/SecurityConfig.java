a747970e3cf20a32bf625d7da69476cb6eaa635b
Daniel Bezden <danvicbez@yahoo.com>
2021-03-29 23:58:22 -0400

    Set up security and start authentication

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.udacity.jwdnd.course1.cloudstorage.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.udacity.jwdnd.course1.cloudstorage.services.AuthenticationService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	AuthenticationService authenticationService;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authenticationService);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/css/**", "/js/**", "/signup", "/login").permitAll()
+				.anyRequest().authenticated()
+				.and().formLogin().loginPage("/login").defaultSuccessUrl("/home", true);
+	}
+}
